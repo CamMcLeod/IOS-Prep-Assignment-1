1238d2a57b3b4f2eae23941910dffc1cad360afd
Cam McLeod <cammcleod3@gmail.com>
2019-05-14 15:50:15 -0700

    changed code to make prettier

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -14,20 +14,18 @@ int main(int argc, const char * argv[]) {
     
     for (i = 1; i<101; i++) {
         
-        if (i % 3 == 0 && i % 5 ==0){
+        if (i % 15 == 0){
             printf("FizzBuzz\n");
-            continue;
-        }
-        if (i % 3 == 0){
+            
+        } else if (i % 3 == 0){
             printf("Fizz\n");
-            continue;
-        }
-        if (i % 5 == 0){
+            
+        } else if (i % 5 == 0){
             printf("Buzz\n");
-            continue;
+            
+        } else {
+            printf("%d\n", i);
         }
-        printf("%d\n", i);
-        
     }
     
     return 0;
